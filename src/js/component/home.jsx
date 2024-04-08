f61c05aec99582def432b470b56dee9aa307eb74
andres fernandes <andresfernandesg10@gmail.com>
2024-04-08 01:37:00 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  
+  const fetchTodos = () => {
+    fetch('https://api.example.com/todos')
+      .then(resp => {
+        if (!resp.ok) {
+          throw new Error('Failed to fetch todos');
+        }
+        return resp.json();
+      })
+      .then(data => setTodos(data))
+      .catch(error => console.error('Error fetching todos:', error));
+  };
+
+  useEffect(() => {
+    
+    fetchTodos();
+  }, []);
+
+ 
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      const updatedTodos = [...todos, { task: newTodo }];
+      setTodos(updatedTodos);
+      syncTodos(updatedTodos);
+      setNewTodo('');
+    }
+  };
+
+  
+  const deleteTodo = todoId => {
+    const updatedTodos = todos.filter((todo, index) => index !== todoId);
+    setTodos(updatedTodos);
+    syncTodos(updatedTodos);
+  };
+
+  
+  const syncTodos = updatedTodos => {
+    fetch('https://api.example.com/todos', {
+      method: "PUT",
+      body: JSON.stringify(updatedTodos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        if (!resp.ok) {
+          throw new Error('Failed to sync todos');
+        }
+        return resp.json();
+      })
+      .then(data => console.log('Todos synced successfully:', data))
+      .catch(error => console.error('Error syncing todos:', error));
+  };
+
+  
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      addTodo();
+    }
+  };
+
+  return (
+    <div className="todo-list-container">
+      <h1 className="todo-list-title">Todo List</h1>
+      <div className="input-container">
+        <input
+          className="todo-input"
+          type="text"
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+          onKeyPress={handleKeyPress} 
+          placeholder="Add new todo"
+        />
+        <button className="add-button" onClick={addTodo}>Add</button>
+      </div>
+      <ul className="todo-items">
+        {todos.map((todo, index) => (
+          <li key={index} className="todo-item">
+            {todo.task}
+            <button className="delete-button" onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
+
+
+
